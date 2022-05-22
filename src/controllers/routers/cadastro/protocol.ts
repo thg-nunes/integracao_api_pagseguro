3c2ac780a976cb76b8cd5d38aabeb50641c8e2ef
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-05-22 13:38:51 -0300

    deleta arquivos nao usados

--- src/controllers/routers/cadastro/protocol.ts
+++
@@ -1,6 +0,0 @@
-import { Request, Response } from "express";
-
-export interface AnalyzeData {
-  analyzePassword?(password: string | number): string | number
-  insertData?(req: Request, res: Response): void
-}
