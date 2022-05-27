448a8a254cb5ef025249072551baba682efac044
thiago nunes <desenvolvedor.nunes@gmail.com>
2022-05-27 16:38:35 -0300

    implementa metodo e rota para buscar todos os usuarios e fazer login

--- src/modules/users/controller/UserController.ts
+++ src/modules/users/controller/UserController.ts
@@ -1,10 +1,14 @@
 import { Request, Response } from "express";
+import { ErrorRequest } from "../../../error/http";
 
 import { User } from "../interfaces";
 
 import { CreateUser } from "../services/CreateUser";
 import { DeleteUser } from "../services/DeleteUser";
 import { GetUser } from "../services/GetUser";
+import { LoginUser } from "../services/LoginUser";
+import { Password } from "../services/Password";
+import { Users } from "../typeorm/entities/Users";
 
 export class UserController {
   async createUser(req: Request, res: Response): Promise<Response> {
@@ -32,5 +36,41 @@ export class UserController {
     const user = await userService.execute(id)
 
     return res.json(user)
+  }
+
+  async getAllUsers(req: Request, res: Response): Promise<Users[] | undefined> {
+    const usersService = new GetUser()
+    const allUsers = await usersService.getAllUsers()
+
+    return allUsers
+  }
+
+  async loginUser(req: Request, res: Response): Promise<Response> {
+    const { email, senha } = req.body
+
+    const loginService = new LoginUser()
+    const emailExists = await loginService.execute(email)
+
+    if(emailExists !== undefined) {
+      const senhaSalva = emailExists.senha
+      const passwordIsCorrect = new Password().comparePassword(senha, senhaSalva)
+
+      if(!passwordIsCorrect) {
+        return res.status(400).json({
+          status: 'failed',
+          message:'Email ou senha incorreta.'
+        })
+      }
+
+      return res.status(200).json({
+        status: 'success',
+        userLogged: true
+      })
+    }
+
+    return res.status(400).json({
+      status: 'failed',
+      message:'Email ou senha incorreta.'
+    })
   }
 }
--- src/modules/users/routes.ts
+++ src/modules/users/routes.ts
@@ -5,7 +5,9 @@ const userRouter = Router()
 const userController = new UserController()
 
 userRouter.get('/:id', userController.getUser)
+userRouter.get('/all', userController.getAllUsers)
 userRouter.post('/create', userController.createUser)
+userRouter.post('/login', userController.loginUser)
 userRouter.delete('/delete/:id', userController.deleteUser)
 
 export default userRouter
